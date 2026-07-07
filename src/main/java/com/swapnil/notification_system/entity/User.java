c1279db1fb19f46379325fee2854c77895bd257d
Swapnil <thakur7swapnil@gmail.com>
2026-07-07 11:10:47 +0530

    created User entity model according to the requirements

---
+++ src/main/java/com/swapnil/notification_system/entity/User.java
@@ -0,0 +1,29 @@
+package com.swapnil.notification_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+}
