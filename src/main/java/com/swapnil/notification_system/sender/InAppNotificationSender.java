f470853c10ebe04a5206d6d8220fd9e3786a3977
Swapnil <thakur7swapnil@gmail.com>
2026-07-08 12:24:03 +0530

    implemented the in app notification sender to simulate notification

--- src/main/java/com/swapnil/notification_system/sender/InAppNotificationSender.java
+++ src/main/java/com/swapnil/notification_system/sender/InAppNotificationSender.java
@@ -4,17 +4,17 @@ import com.swapnil.notification_system.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PushNotificationSender implements NotificationSender{
+public class InAppNotificationSender implements NotificationSender{
+
     @Override
     public void send(User user, String title, String body) {
-        System.out.println("PUSH NOTIFICATION");
+
+        System.out.println("IN APP NOTIFICATION");
 
         System.out.println("User ID: " + user.getId());
 
         System.out.println("Title: " + title);
 
         System.out.println("Message: " + body);
-
-
     }
 }
